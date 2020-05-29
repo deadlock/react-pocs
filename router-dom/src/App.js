d844e2aac4d538e996ec539b79f1c392d7501985
diogo.silva <digospyboss@gmail.com>
2020-05-29 13:29:26 -0300

    adding guards

--- router-dom/src/App.js
+++ router-dom/src/App.js
@@ -10,6 +10,10 @@ import Courses from "./containers/Courses/Courses";
 import Users from "./containers/Users/Users";
 
 class App extends Component {
+  state = {
+    auth: false,
+  };
+
   render() {
     return (
       <BrowserRouter>
@@ -26,8 +30,11 @@ class App extends Component {
                 <li style={{ marginRight: "10px" }}>
                   <NavLink to="/courses">Courses</NavLink>
                 </li>
-                <li>
+                <li style={{ marginRight: "10px" }}>
                   <NavLink to="/users">Users</NavLink>
+                </li>
+                <li>
+                  <NavLink to="/admin">Admin Area</NavLink>
                 </li>
               </ul>
             </nav>
@@ -37,6 +44,11 @@ class App extends Component {
             <Redirect from="/all-courses" to="/courses" />
             <Route path="/courses" component={Courses} />
             <Route path="/users" component={Users} />
+            {this.state.auth ? (
+              <Route path="/admin" render={() => <h1>Admin Area</h1>} />
+            ) : (
+              <Redirect from="/admin" to="/users" />
+            )}
             <Route render={() => <h1>401 - Page not found</h1>} />}
             <Route path="/" />
           </Switch>
